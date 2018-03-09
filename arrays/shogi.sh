cb0e413115a79defdf897fd46613e072f6d48bf4
azimut <azimut@example.com>
2018-03-09 01:33:11 -0300

    add shogi

---
+++ arrays/shogi.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# https://en.wikipedia.org/wiki/Shogi
+declare -A things
+things=(
+[王將]='King'
+[飛車]='Rook'
+[角行]='Bishop'
+[金將]='Gold general'
+[銀將]='Silver general'
+[桂馬]='Knight'
+[香車]='Lance'
+[歩兵]='Pawn'
+)
